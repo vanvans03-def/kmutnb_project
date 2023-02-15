0b56367cbdef7f8f4be08dbc6b4ddf7fc09794e7
TEERA HITCHANA <teerahitchana@gmail.com>
2023-02-15 11:16:04 +0700

    add Thai Language and do fix map json Product

--- lib/features/admin/services/admin_service.dart
+++ lib/features/admin/services/admin_service.dart
@@ -8,30 +8,40 @@ import 'package:kmutnb_project/models/product.dart';
 class AdminService {
   void sellProduct({
     required BuildContext context,
-    required String name,
-    required String description,
-    required double price,
-    required double quantity,
+    required String productName,
     required String category,
-    required List<File> images,
+    required String productShortDescription,
+    required double productPrice,
+    required double productSalePrice,
+    required List<File> productImage,
+    required String productSKU,
+    required String productType,
+    required String stockStatus,
+    required String relatedProduct,
+    required String id,
   }) async {
     try {
       final cloundinary = CloudinaryPublic('dp6dsdn8y', 'x2sxr5vn');
       List<String> imageUrls = [];
 
-      for (int i = 0; i < images.length; i++) {
+      for (int i = 0; i < productImage.length; i++) {
         CloudinaryResponse res = await cloundinary.uploadFile(
-          CloudinaryFile.fromFile(images[i].path, folder: name),
+          CloudinaryFile.fromFile(productImage[i].path, folder: productName),
         );
         imageUrls.add(res.secureUrl);
       }
       Product product = Product(
-        name: name,
-        description: description,
-        quantity: quantity,
-        images: imageUrls,
+        productName: productName,
         category: category,
-        price: price, //5.28
+        productShortDescription: productShortDescription,
+        productPrice: productPrice,
+        productSalePrice: productSalePrice, //5.28
+        productImage: imageUrls,
+        productSKU: productSKU,
+        productType: productType,
+        stockStatus: stockStatus,
+        relatedProduct: relatedProduct,
+        id: id,
       );
     } catch (e) {
       showSnackBar(context, e.toString());
